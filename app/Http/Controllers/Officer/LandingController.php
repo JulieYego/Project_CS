dfd5aaaf9e1ac6688d2f08bc2321376edec1dcdb
JulieYego <julie.yego@strathmore.edu>
2021-08-11 12:32:53 +0300

    My commit

--- app/Http/Controllers/Officer/LandingController.php
+++ app/Http/Controllers/Officer/LandingController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\DB;
+use App\Models\user;
+use Auth;
 
 class LandingController extends Controller
 {
@@ -17,6 +19,12 @@ class LandingController extends Controller
         //return view('officers.officer_landing_page');
         return View::make('officers\officer_landing_page');
 
+    }
+
+    public function profile() {
+        //return view('officers.officer_landing_page');
+        $officers = Auth::user();
+        return view('officers.profile')->with('officers',$officers);
     }
 
     
