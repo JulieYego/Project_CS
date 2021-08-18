7a1ec1ed78e1f2adf63dac1704a3a54dd8d4223b
Nelimzzz <lulunelima@gmail.com>
2021-08-18 12:42:49 +0300

    Officer

--- resources/views/ocs/edit.blade.php
+++ resources/views/ocs/edit.blade.php
@@ -64,7 +64,7 @@
     
 
             <button type="submit" class="btn btn-warning" >
-                Update
+                Update Officer
             </button>
 
 
@@ -73,5 +73,16 @@
 
     </div>
 </body>
-
+<!--<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>-->
 </html>
